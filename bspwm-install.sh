baab5b073e0156993fd9e885349ff74152a2b7e1
ghost <arias0723@gmail.com>
2022-02-25 17:54:03 -0300

    Fixed install file

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -10,6 +10,7 @@ chmod -R +x ~/.config/bspwm
 chmod -R +x ~/.config/polybar/scripts
 packages=`grep -vE "^#" arch-packages.txt` 
 sudo pacman -Syu --needed --noconfirm $packages
+sudo systemctl enable NetworkManager.service
 sudo fc-cache -f -v
 dbus-launch dconf load / < xed.dconf
 
