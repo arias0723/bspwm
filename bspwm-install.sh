51b8a6c4a5549335608d6b1d2cc2c8c2d602c6e6
ghost <arias0723@gmail.com>
2022-02-25 16:26:55 -0300

    Initial install structure

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -3,9 +3,12 @@
 # Easy setup for testing
 mkdir ~/.local/share/fonts
 cp -R IosevkaTermNerdFontComplete.ttf ~/.local/share/fonts
+mkdir ~/Pictures
+cp wallpapers/* ~/Pictures
 cp -R .config/* ~/.config/
 chmod -R +x ~/.config/bspwm
 chmod -R +x ~/.config/polybar/scripts
-sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
+grep -vE "^#" arch-packages.txt | sudo pacman -Syu --needed --noconfirm
 sudo fc-cache -f -v
 dbus-launch dconf load / < xed.dconf
+
