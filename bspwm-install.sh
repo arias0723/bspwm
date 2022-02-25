1095a8c86519af48ed282ac66c1d9db6d36ab1fb
ghost <arias0723@gmail.com>
2022-02-25 18:10:12 -0300

    Fixed install file

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Easy setup for testing
+mkdir -p ~/.config
 mkdir -p ~/.local/share/fonts
-cp IosevkaTermNerdFontComplete.ttf ~/.local/share/fonts
 mkdir ~/Pictures
+
+cp IosevkaTermNerdFontComplete.ttf ~/.local/share/fonts
 cp wallpapers/* ~/Pictures
 cp -R .config/* ~/.config/
 cp -R .screenlayout ~/
 chmod -R +x ~/.config/bspwm
 chmod -R +x ~/.config/polybar/scripts
 chmod -R +x ~/.screenlayout
+
 packages=`grep -vE "^#" arch-packages.txt` 
 sudo pacman -Syu --needed --noconfirm $packages
 sudo systemctl enable NetworkManager.service
